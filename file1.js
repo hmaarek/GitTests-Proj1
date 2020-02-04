5752cef30d948f5b11799044fe2a0675cb1b5086
Hossam Mostafa <hosammaarek@gmail.com>
2020-02-04 15:42:49 +0300

    initial project version

---
+++ file1.js
@@ -0,0 +1,16 @@
+
+/*
+* File 1
+* A test file to learn Git
+*/
+
+
+//This is file 1
+// Version 1.0
+
+#include lib1
+
+void main()
+{
+	print "somrthing here"
+}
---
+++ file2.js
@@ -0,0 +1,19 @@
+
+/*
+* File 2
+* A test file to learn Git
+*/
+
+
+//This is file 2
+// Version 1.0
+
+#include lib2
+
+void func(int a, int b)
+{
+	int c;
+
+	c = a +b;
+	print "C = " ; c
+}
